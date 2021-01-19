87ecce0d28dd2b5a3a79921f2646ece13f5b254c
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-01-19 10:32:59 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,24 @@
 #include "postfix.h"
 
 int main() {
-    //std::string s1("2 + 6 * 3 / (4 - 2)");
-    //std::string s2 = infix2postfix(s1);
-    //std::cout << s2;  // 2 6 3 * 4 2 - / +
+    std::string s1("2 + 6 * 3 / (4 - 2)");
+    std::string s2 = infix2postfix(s1);
+    std::cout << s2;  // 2 6 3 * 4 2 - / +
 
-    std::string inf = "2 + 2";
-    std::string postf = infix2postfix(inf);
-    std::cout << postf; //"2 2 +";
+    std::string inf_1 = "123 + 15 + 10";
+    std::string postf_1 = infix2postfix(inf_1);
+    std::cout << postf_1; //"123 15 10 + +";
 
-    //std::string inf = "2 + 6 * 3 / (4 - 2)";
-    //std::string postf = infix2postfix(inf);
-    //std::cout << postf; // "2 6 3 * 4 2 - / +";
+    std::string inf_2 = "1 + 2 * 3 / (4 - 5)";
+    std::string postf_2 = infix2postfix(inf_2);
+    std::cout << postf_2; // "1 2 3 * 4 5 - / +";
 
+    std::string inf_3 = "(0.1 + 0.9) * (4.1 - 0.1)";
+    std::string postf_3 = infix2postfix(inf_3);
+    std::cout << postf_3; // "0.1 0.9 + 4.1 0.1 - *";
 
-    //std::string inf = "(2 + 8.3) * (6 - 3.2)";
-    //std::string postf = infix2postfix(inf);
-    //std::cout << postf; // "2 8.3 + 6 3.2 - *";
-
-    //std::string inf = "(6 * (4 - 2) + 5) * (2.6 + 3 * 7) - 9";
-    //std::string postf = infix2postfix(inf);
-    //std::cout << postf; // "6 4 2 - * 5 + 2.6 3 7 * + * 9 -";
+    std::string inf_4 = "(1 * (2 * (3 * (4 * (5 * (6 * (7 * (8 * (9 * (10 + 1))))))))))";
+    std::string postf_4 = infix2postfix(inf_4);
+    std::cout << postf_4; // "1 2 3 4 5 6 7 8 9 10 1 + * * * * * * * * *";
     return 0;
 }
